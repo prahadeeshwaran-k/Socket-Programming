084b784093386a51c4f716397ed8ef0bc9668a22
prahadeeshwaran-k <prahadeeshwaran.mk@gmail.com>
2025-12-01 23:35:14 +0530

    Select Function

---
+++ 07.Select Function/client.c
@@ -0,0 +1,59 @@
+// Simple TCP Client.
+// Vector class code
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int sockFD;
+    struct sockaddr_in server_address;
+    char buffer[128];
+
+    // 1. Create socket
+    sockFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockFD < 0) {
+        perror("socket");
+        return 0;
+    }
+
+    printf("Socket created with file descriptor %d\n", sockFD);
+
+    // 2. Fill server address
+    bzero(&server_address,sizeof(server_address));
+    server_address.sin_family      = AF_INET;
+    server_address.sin_port        = htons(8080);
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");//loopback ip address
+
+    // 3. Connect to server
+    if (connect(sockFD, (struct sockaddr *)&server_address, sizeof(server_address)) == 0) {
+        printf("Connect Successfully\n");
+    } else {
+        perror("connect");
+        close(sockFD);
+        return 0;
+    }
+
+    // 4. Send data in loop
+    while (1) {
+        printf("Enter Data: ");
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            printf("EOF or input error, exiting...\n");
+            break;
+        }
+
+        // send without the extra +1 if you don't want to send the '\0'
+        ssize_t n = write(sockFD, buffer, strlen(buffer));
+        if (n < 0) {
+            perror("write");
+            break;
+        }
+    }
+
+    close(sockFD);
+    return 0;
+}
---
+++ 07.Select Function/server.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+
+int main()
+{
+    int sfd, newsfd, i;
+    struct sockaddr_in server, client;
+    char buf[120];
+
+    fd_set rdset, testset; // for the select File Descriptor
+    socklen_t len;
+
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd < 0)
+    {
+        perror("socket");
+        return 1;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(2000);
+    server.sin_addr.s_addr = inet_addr("0.0.0.0");
+
+    if (bind(sfd, (struct sockaddr *)&server, sizeof(server)) == 0)
+    {
+        printf("Bind success\n");
+    }
+    else
+    {
+        perror("bind");
+        return 1;
+    }
+
+    listen(sfd, 5); // how many client can be handle
+
+    len = sizeof(client);
+    FD_ZERO(&rdset);     // this makes all 1024 bits to zero in rdset
+    FD_SET(sfd, &rdset); // here sfd (file descriptor number) equal to bit position is set to monitor the FD
+
+    while (1)
+    {
+        testset = rdset;                         // take the copy of rdset(it holds all the FD in a single array with bit positions)
+        select(100, &testset, NULL, NULL, NULL); // 100 -> how many file descriptor it should monitor (0 to 99) file descriptor
+
+        for (i = 0; i < 100; i++)
+        {
+            if (FD_ISSET(i, &testset)) // check weather particular bit is set or not in testset
+                break;                 // here it clear all the bit except the bit which send request.
+        }
+
+        // i == sfd means i ie equal to server FD , which tells it is a new connection request 
+        if (i == sfd)//check FD number = bit position, if fail then it move to else part,
+        {            //if failmeans then the request information already existing client.
+            printf("New connection\n");
+            newsfd = accept(sfd, (struct sockaddr *)&client, &len);
+            FD_SET(newsfd, &rdset); // the new connection to the existing rdset bits
+        }
+        else
+        {
+            bzero(buf, 120);
+            if (read(i, buf, 120) > 0)
+                printf("%s\n", buf);
+            else
+            {
+                close(i);
+                FD_CLR(i, &rdset);
+            }
+        }
+    }
+    return 0;
+}
